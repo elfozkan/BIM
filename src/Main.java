4a843184ccfb661efd9aed4daff24cd375dc32f2
Elif Özkan <117855706+elfozkan@users.noreply.github.com>
2022-12-02 11:36:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //Vücut Kitle İndeksi Hesaplama
+        //kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın.
+        //formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
+        //Kilo (kg) / Boy(m) * Boy(m)
+        //Lütfen boyunuzu (metre cinsinde) giriniz : 1,72
+        //Lütfen kilonuzu giriniz : 105
+        //Vücut Kitle İndeksiniz : 35.49215792320173
+    }
+}
