e98bc2529dff3579d1cca933f858987ded99c231
Elif Özkan <117855706+elfozkan@users.noreply.github.com>
2022-12-02 12:06:55 +0300

    bim kod yazildi

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //Vücut Kitle İndeksi Hesaplama
-        //kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın.
-        //formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
-        //Kilo (kg) / Boy(m) * Boy(m)
-        //Lütfen boyunuzu (metre cinsinde) giriniz : 1,72
-        //Lütfen kilonuzu giriniz : 105
-        //Vücut Kitle İndeksiniz : 35.49215792320173
+        //kullanıcıdan boy ve kilo değerlerini alan "Vücut Kitle İndeks (BIM)" değerini hesaplayan program.
+        double kg , m , bim ;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz: ");
+        m = input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        kg = input.nextDouble();
+        bim = kg/Math.pow(m,2) ;
+        System.out.println("Vücut Kitle İndeksiniz: "+bim);
+
     }
 }
